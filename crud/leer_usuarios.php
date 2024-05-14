551da66dc0a456ca997fa7d35fe28081366f4168
Francisco Pérez Mogollón <59151009+franperezec@users.noreply.github.com>
2024-05-13 20:57:53 -0500

    proyecto CRUD mejora tabla base

--- crud/leer_usuarios.php
+++ crud/leer_usuarios.php
@@ -5,12 +5,32 @@ $sql = "SELECT * FROM usuarios";
 $result = mysqli_query($conexion, $sql);
 
 if (mysqli_num_rows($result) > 0) {
+    echo "<table border='1'>
+            <tr>
+                <th>Cédula</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Correo</th>
+                <th>Teléfono</th>
+                <th>Rol</th>
+                <th>Fecha de Registro</th>
+            </tr>";
     while($row = mysqli_fetch_assoc($result)) {
-        echo "Cédula: " . $row["cedula"]. " - Nombre: " . $row["nombre"]. " - Apellido: " . $row["apellido"]. " - Correo: " . $row["correo"]. " - Teléfono: " . $row["telefono"]. " - Rol: " . $row["rol"]. " - Fecha de Registro: " . $row["fecha_registro"]. "<br>";
+        echo "<tr>
+                <td>" . $row["cedula"] . "</td>
+                <td>" . $row["nombre"] . "</td>
+                <td>" . $row["apellido"] . "</td>
+                <td>" . $row["correo"] . "</td>
+                <td>" . $row["telefono"] . "</td>
+                <td>" . $row["rol"] . "</td>
+                <td>" . $row["fecha_registro"] . "</td>
+            </tr>";
     }
+    echo "</table>";
 } else {
     echo "0 resultados";
 }
 
 mysqli_close($conexion);
 ?>
+
